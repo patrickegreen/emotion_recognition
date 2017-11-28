db62cd706a921a5aa380516d1adc3f19c841faa4
patrickegreen <patrickegreen@gmail.com>
2017-11-27 19:06:58 -0500

    Raw images in grayscale

--- interestPoints/converter.m
+++ interestPoints/converter.m
@@ -6,13 +6,13 @@ labels = vertcat(data{:, 2});
 raw_count = size(labels, 1);
 remove_idxs = [];
 sample_count = size(labels, 1);    % count of valid samples
-root = 'data/close_crop';
+root = 'data/raw';
 for i = 1:raw_count
     sample_name = names(i);
     filename = sprintf('%s/%s', root, sample_name);
     face = double(imread(filename));
     face = face ./ double(max(face(:)));
     face = rgb2gray(face);
-    imwrite(face, sprintf('data/close_crop_gray/%s', sample_name));
+    imwrite(face, sprintf('data/raw_gray/%s', sample_name));
 end
 
